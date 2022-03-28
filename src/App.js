4f1d343c49b56138a67881156748615f572fe1a3
JubayerDev <ajashes858@gmail.com>
2022-03-28 14:14:02 +0600

    text animation added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
 import MyLineChart from './components/MyLineChart/MyLineChart';
 import SpecialChart from './components/SpecialChart/SpecialChart';
-import { useSpring, animated } from 'react-spring'
+import { useSpring, animated, config } from 'react-spring'
+import { useState } from 'react';
 
 function App() {
-  const props = useSpring({ to: { opacity: 1 }, from: { opacity: 0 } })
+  const [flip, set] = useState(false)
+  const props = useSpring({
+    to: { opacity: 1 }, from: { opacity: 0 }, reset: true, reverse: flip, delay: 200,config: config.molasses,
+    onRest: () => set(!flip),
+  })
   return (
     <div className="App">
       <animated.h1 style={props}>This is React Spring</animated.h1>
