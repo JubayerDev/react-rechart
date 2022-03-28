721c165ac138dd02735ac4ab1ae4cc44e90b29ff
JubayerDev <ajashes858@gmail.com>
2022-03-28 14:04:29 +0600

    react-spring animation added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
 import MyLineChart from './components/MyLineChart/MyLineChart';
 import SpecialChart from './components/SpecialChart/SpecialChart';
+import { useSpring, animated } from 'react-spring'
 
 function App() {
+  const props = useSpring({ to: { opacity: 1 }, from: { opacity: 0 } })
   return (
     <div className="App">
+      <animated.h1 style={props}>This is React Spring</animated.h1>
       <MyLineChart></MyLineChart>
       <SpecialChart></SpecialChart>
     </div>
